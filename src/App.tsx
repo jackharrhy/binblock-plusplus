6cf19c3e9419981762e266c8750e901633b08576
Jack Arthur Harrhy <me@jackharrhy.com>
2025-12-26 16:22:12 -0330

    better size

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,12 @@ export function App() {
 
   return (
     <main className="flex w-screen h-screen overflow-hidden">
-      <aside className="w-64 h-full shrink-0 bg-black/10" />
+      <aside className="w-[16rem] h-full shrink-0 bg-black/10" />
       <div
         ref={handleContainerRef}
         className="flex-1 h-full min-w-0 overflow-hidden"
       />
-      <aside className="w-64 h-full shrink-0 bg-black/10" />
+      <aside className="w-[16rem] h-full shrink-0 bg-black/10" />
     </main>
   );
 }
